9b1bcd9a9a3a8e9166e5cb94187fc2b44227e456
prajwal934 <samalprajwal.302@gmail.com>
2025-03-08 20:32:23 +0530

    adding ServiceRegistry

---
+++ ServiceRegistry/src/test/java/com/praj/hotel/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praj.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
